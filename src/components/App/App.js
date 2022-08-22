c933d2559b40c74b54d60ea01ee5759e3ad3b469
Monogato <monogato.dg@gmail.com>
2022-08-22 16:50:02 -0500

    Siguiendo la regla de Statefull & Stateless

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,7 @@
 //import './App.css';
 import React from "react";
 import "./App.css"
-import { TodoCounter } from "../TodoCounter/TodoCounter.js";
-import { TodoSearch } from "../TodoSearch/TodoSearch.js";
-import { TodoList } from "../TodoList/TodoList.js";
-import { TodoItem } from "../TodoItem/TodoItem.js";
-import { CreateTodoButton } from "../CreateTodoButton/CreateTodoButton.js";
+import { AppUI } from "./AppUI";
 
 
 const defaultTodos = [
@@ -60,28 +56,15 @@ function App() {
   }
 
   return (
-    <React.Fragment>
-      <TodoCounter 
-        total={totalTodos}
-        completed={completedTodos}
-      />    
-      <TodoSearch 
-        searchValue = {searchValue}
-        setSearchValue = {setSearchValue}
-      />
-      <TodoList>
-        {searchedTodos.map(todo => (
-            <TodoItem 
-              key={todo.text} 
-              text={todo.text}
-              completed={todo.completed} 
-              onComplete={() => completeTodo(todo.text)}
-              onDelete={() => deleteTodo(todo.text)}
-            />
-        ))}
-      </TodoList>
-      <CreateTodoButton />      
-   </React.Fragment>
+    < AppUI 
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+      searchedTodos = {searchedTodos}
+      completeTodo = {completeTodo}
+      deleteTodo = {deleteTodo}
+    />
   );
 }
 
